c7703c55cb5c1abcf4e0843047a1d36021db16e3
Carlos Alberto <dalbert9615@gmail.com>
2024-06-13 12:22:28 +0200

    Add sensor servlet

--- backend/src/main/java/Controller/Sensor.java
+++ backend/src/main/java/Controller/Sensor.java
@@ -8,7 +8,11 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.util.List;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import com.google.gson.Gson;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @WebServlet(name = "Sensor", urlPatterns = {"/sensor"})
 public class Sensor extends HttpServlet {
@@ -16,8 +20,14 @@ public class Sensor extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        List<CoordinatesDTO> listCoordinates = new CoordinatesDAO().getCoords();
-        request.setAttribute("listCoordinates",listCoordinates);
+        try {
+            ArrayList<CoordinatesDTO> listCoordinates;
+            listCoordinates = new CoordinatesDAO().getCoords();
+            String json = new Gson().toJson(listCoordinates);
+            response.getWriter().write(json);
+        } catch (SQLException ex) {
+            Logger.getLogger(Sensor.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
