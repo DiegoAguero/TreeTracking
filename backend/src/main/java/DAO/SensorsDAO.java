3f7c86712060d3ac04d73656e9fad99e1c04dcac
Carlos Alberto <dalbert9615@gmail.com>
2024-06-13 14:23:54 +0200

    Erase comments

--- backend/src/main/java/DAO/SensorsDAO.java
+++ backend/src/main/java/DAO/SensorsDAO.java
@@ -1,12 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package DAO;
 import DTO.SensorsDTO;
 import java.sql.*;
 import java.util.*;
-import DAO.ConnectionDAO;
 import DTO.CoordinatesDTO;
 
 public class SensorsDAO {
