8f52186b864d6b6eaebdc7604bf34c25ee8cb90f
Carlos Alberto Diaz B <carlos.alberto.diaz@estudiantat.upc.edu>
2024-06-22 13:17:47 +0200

    Untracking sensible data

--- frontend/src/environments/environments.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-  TOKEN_MAP_BOX: 'pk.eyJ1IjoiamFhdnYiLCJhIjoiY2xycGFsb3U2MDRzNTJybm1qanJ2YzBpeCJ9.fj9jqKOSW3JQXwKUCOn8Zg',
-  URL_BASE: 'https://api.mapbox.com/geocoding/v5/mapbox.places',
-}
-
-
-Object.freeze(environment);
